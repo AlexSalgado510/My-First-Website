3ec769bc106c1a8d50a5d9aad9adbd76df4aa429
AlexSalgado510 <100883615+AlexSalgado510@users.noreply.github.com>
2022-03-23 21:13:46 -0500

    Update script.js
    This was code from a separate project with Thinkful and had nothing to do with this project.

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function myGrades(score) {
-  let sum = 0;
-  for ( let i = 0; i < myGrades.length; i++) {
-    sum = myGrades[i];
-  }
-  return score;
-} 
 
-function campsiteCount(camgrounds) {
-  let counter = 0;
-  for (let i = 0; i < campgrounds.length; i++) {
-    counter = campgrounds.length;
-  }
-  return counter;
-} 
